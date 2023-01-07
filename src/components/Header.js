8420a9f95f1b0abd81f41599fed6e7297a38d830
bri <bcamesi@gmail.com>
2023-01-07 12:31:25 -0500

    render header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
-function Header (props) {
-    return <h1>Header Name</h1>
+import { Link } from "react-router-dom";
+
+function Header(props){
+    return <nav className="nav">
+        <Link to="/">
+            <div>People App</div>
+        </Link>
+    </nav>
 }
 
 export default Header
