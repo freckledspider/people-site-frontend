76cf469718f8889fa5e589f30a10221dd646a499
bri <bcamesi@gmail.com>
2023-01-07 16:11:02 -0500

    Post method and forms created

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,23 @@
-const URL = "https://people-api-zs31.onrender.com"
+import { redirect } from "react-router-dom";
 
-export const createAction = async ({request}) => {
-    const formData = await request.formData
-     const newPerson = {
+const URL = "https://people-api-zs31.onrender.com";
+
+export const createAction = async ({ request }) => {
+    const formData = await request.formData();
+
+    const newPerson = {
         name: formData.get("name"),
         image: formData.get("image"),
         title: formData.get("title"),
-     }
-}
+    };
+
+    await fetch(URL + "/people", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPerson),
+    });
+
+    return redirect("/");
+};
