ca510ca0c44b2ac25f357dad701de210ba19e776
bri <bcamesi@gmail.com>
2023-01-10 21:42:41 -0500

    update, delete, and styling

--- src/actions.js
+++ src/actions.js
@@ -4,7 +4,7 @@ const URL = "https://people-api-zs31.onrender.com";
 
 export const createAction = async ({ request }) => {
     const formData = await request.formData();
-
+    console.log(formData);
     const newPerson = {
         name: formData.get("name"),
         image: formData.get("image"),
@@ -21,3 +21,30 @@ export const createAction = async ({ request }) => {
 
     return redirect("/");
 };
+
+export const updateAction = async ({ request, params }) => {
+    const formData = await request.formData();
+    const updatedPerson = {
+        name: formData.get("name"),
+        image: formData.get("image"),
+        title: formData.get("title"),
+    };
+
+    await fetch(URL + "/people/" + params.id, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedPerson),
+    });
+
+    return redirect("/");
+};
+
+export const deleteAction = async ({params}) => {
+    await fetch(URL + "/people/" + params.id, {
+        method: "delete"
+    })
+
+    return redirect("/")
+}
--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import App from './App';
 import { peopleLoader, personLoader } from './loaders';
-import {createAction} from "./actions";
+import {createAction, updateAction, deleteAction} from "./actions";
 import Index from './pages/Index';
 import Show from './pages/Show';
 
@@ -16,8 +16,8 @@ const router = createBrowserRouter(
                 <Route path="" element={<Index/>} loader={peopleLoader}/>
                 <Route path=":id" element={<Show/>} loader={personLoader}/>
                 <Route path="create" action={createAction}/>
-                <Route path="update/:id"/>
-                <Route path="delete/:id"/>
+                <Route path="update/:id" action={updateAction}/>
+                <Route path="delete/:id" action={deleteAction}/>
         </Route>
 
     )
